6e52a045e6c6f26206cec6e8bae6390ed9d585c8
Giulia <giulialosito94@gmail.com>
2020-06-25 15:58:06 +0200

    Correzione

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,30 +1,3 @@
-// var lista;
-// var listaPrima;
-//
-//
-// for (var i = 1; i <= 100; i++) {
-//   lista = i;
-//
-//   if (i%3 == 0 && i%5 == 0) {
-//     lista = i;
-//     listaPrima = document.getElementById('lista').innerHTML;
-//     document.getElementById("lista").innerHTML= listaPrima + "<li>" + "FizzBuzz" + "</li>" ;
-//   } else if (i%5 == 0) {
-//     lista = i;
-//     listaPrima = document.getElementById('lista').innerHTML;
-//     document.getElementById("lista").innerHTML= listaPrima + "<li>" + "Buzz" + "</li>" ;
-//   } else if (i%3 == 0) {
-//     lista = i;
-//     listaPrima = document.getElementById('lista').innerHTML;
-//     document.getElementById("lista").innerHTML= listaPrima + "<li>" + "Fizz" + "</li>" ;
-//   } else {
-//     lista = i;
-//     listaPrima = document.getElementById('lista').innerHTML;
-//     document.getElementById("lista").innerHTML= listaPrima + "<li>" + lista + "</li>" ;
-//   }
-//
-// }
-
 var lista = document.getElementById('lista');
 
 for (var i = 1;i <= 100; i++) {
